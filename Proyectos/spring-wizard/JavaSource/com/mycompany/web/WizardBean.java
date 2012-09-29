39d05f348e730c7175e86bc5015215c63af6ccc1
Jose Diaz <jose.diaz@joedayz.pe>
2012-09-29 17:31:31 -0500

    JSFUtils y WizardBean

---
+++ Proyectos/spring-wizard/JavaSource/com/mycompany/web/WizardBean.java
@@ -0,0 +1,76 @@
+package com.mycompany.web;
+
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+
+import org.hibernate.validator.Email;
+import org.hibernate.validator.NotEmpty;
+import org.hibernate.validator.Pattern;
+
+import com.mycompany.service.OrderService;
+import com.mycompany.utils.JSFUtils;
+
+public class WizardBean implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Pattern(regex = ".*[^\\s].*", message = "This string contain only spaces")
+    private String name;
+
+    @NotEmpty
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String drink;
+    private String comments;
+    private String drinkCategorySelected;
+   
+    private OrderService orderService;
+
+    private String startPage;
+   
+    private List<SelectItem> drinkCategory;
+    private List<SelectItem> drinkList;
+
+    // init all drinks
+    private static final String [] CATEGORY = {"Brandy", "Rum", "Tequila", "Whiskey", "Wine", "Beer"};
+    private static final String [] BRANDY = {"XO", "VSOP", "VS"};
+    private static final String [] RUM = {"Medium", "Full-bodied", "Aromatic"};
+    private static final String [] TEQUILA = {"Reposado", "A–ejo", "Blanco"};
+    private static final String [] WHISKEY = {"Malt", "Grain", "Single Malt", };
+    private static final String [] WINE = {"Red", "White", "Pink"};
+    private static final String [] BEER = {"Ales", "Lager", "Specialty Beer", };
+   
+    public void changeDrink(ValueChangeEvent event) {
+        String newValue = (String) event.getNewValue();
+       
+        if (newValue.equals("Brandy")) {drinkList = JSFUtils.createList(BRANDY); drink=BRANDY[0];}
+        else if (newValue.equals("Rum")) {drinkList = JSFUtils.createList(RUM); drink=RUM[0];}
+        else if (newValue.equals("Tequila")) {drinkList = JSFUtils.createList(TEQUILA);drink=TEQUILA[0];}
+        else if (newValue.equals("Whiskey")) {drinkList = JSFUtils.createList(WHISKEY);drink=WHISKEY[0];}
+        else if (newValue.equals("Wine")) {drinkList = JSFUtils.createList(WINE);drink=WINE[0];}
+        else if (newValue.equals("Beer")) {drinkList = JSFUtils.createList(BEER);drink=BEER[0];}
+    }
+   
+    @PostConstruct
+    public void create() {
+        drinkCategorySelected = CATEGORY[0];
+        drinkCategory = JSFUtils.createList(CATEGORY);
+        drinkList = JSFUtils.createList(BRANDY);
+        drink = BRANDY[0];
+    }
+    public void save() {
+        orderService.addOrder(name, email, drink, comments);
+        this.startPage = "/page1.xhtml";
+
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap()
+                .remove("wizardBean");
+    }
+
+} 
